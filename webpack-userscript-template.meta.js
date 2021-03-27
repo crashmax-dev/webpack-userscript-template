b34ef565ad0d25df8311a655c1375fa55fcf8340
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-27 05:22:37 +0000

    Deploy crashmax-off/webpack-userscript-template to github.com/crashmax-off/webpack-userscript-template.git:gh-pages

---
+++ webpack-userscript-template.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        webpack-userscript-template
+// @version     1.1.2
+// @author      Vitalij Ryndin
+// @description 🚀 Userscript developed with modern tech powered by webpack
+// @homepage    https://crashmax-off.github.io/webpack-userscript-template/
+// @include     /^(http|https)://(example.com|example.org|example.edu).*$/
+// ==/UserScript==
