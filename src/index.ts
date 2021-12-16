bb96a7e7b93c8a504079c265a5c3050430206413
Vitalij Ryndin <sys@crashmax.ru>
2021-12-16 21:09:00 +0800

    refactor example

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,37 @@
 import { el, mount } from 'redom'
 import fetcher from './utils/fetcher'
+import './global.scss'
 
-// import './style.css'
-import './style.scss'
+(async () => {
+  /** RE:DOM */
+  const logo = el('img', {
+    src: BASE_PATH + 'typescript.png',
+    style: {
+      display: 'block',
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  })
 
-/**
- * DOM Manipulation
- */
-const logo = el('img', {
-  src: BASE_PATH + 'typescript.png',
-  style: {
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto'
+  mount(document.querySelector('body > div')!, logo)
+
+  /** fetch example */
+  interface ITodos {
+    completed: boolean
+    id: number
+    title: string
+    userId: number
   }
-})
 
+  const response = await fetcher<ITodos>(
+    'https://jsonplaceholder.typicode.com/todos/10',
+    { method: 'GET' }
+  )
 
-mount(document.querySelector('body > div')!, logo)
-
-const title = el('h1', {
-  innerHTML: '🚀 Typescript + Webpack userscript template!',
-  style: {
-    cursor: 'pointer'
-  },
-  onclick: function () {
-    console.log(this)
-  }
-})
-
-const h1 = document.querySelector('h1')
-h1!.replaceWith(title)
-
-const p = document.querySelector('p')
-if (p) {
-  p.textContent = 'This template is based on Webpack and webpack-userscript, written in TypeScript.'
-  p.appendChild(el('a', {
-    href: 'https://github.com/crashmax-off/webpack-userscript-template'
-  }))
-}
-
-/** fetch example */
-interface ITodos {
-  completed: boolean
-  id: number
-  title: string
-  userId: number
-}
-
-fetcher<ITodos>(
-  'https://jsonplaceholder.typicode.com/todos/10',
-  { method: 'GET' }
-).then((response) => {
   console.log(response)
-})
 
-/** webpack define */
-console.log(DEV_MODE)
-console.log(BASE_PATH)
-console.log(APP_VERSION)
+  /** webpack define */
+  console.log(VERSION)
+  console.log(NODE_ENV)
+  console.log(BASE_PATH)
+})()
