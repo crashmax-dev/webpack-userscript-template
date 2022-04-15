c6e1e3fe53811703956d9e6e3f681a0f58564291
crashmax-dev <crashmax.dev@gmail.com>
2022-04-15 19:03:05 +0000

    Deploying to gh-pages from @ crashmax-dev/webpack-userscript-template@193d88abef3a5fc8ed722059121fc4d6eb24b2dd 🚀

--- webpack-userscript-template.user.js
+++ webpack-userscript-template.user.js
@@ -4,6 +4,8 @@
 // @author      Vitalij Ryndin
 // @description webpack-userscript-template
 // @homepage    https://crashmax-dev.github.io/webpack-userscript-template/
+// @updateURL   https://crashmax-dev.github.io/webpack-userscript-template/webpack-userscript-template.meta.js
+// @downloadURL https://crashmax-dev.github.io/webpack-userscript-template/webpack-userscript-template.user.js
 // @include     /^(http|https)://(example.com|example.org|example.edu).*$/
 // ==/UserScript==
 
